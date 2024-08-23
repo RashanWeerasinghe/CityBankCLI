76a4fe4b1730ac13045fe6613b96f3e053be46f0
RashanWeerasinghe <malithrashan03@gmail.com>
2024-08-23 21:23:05 +0530

    set db connection

---
+++ CityBank/src/main/java/org/example/DatabaseCon.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseCon {
+    String DB_URL = "jdbc:mysql://localhost:3306/citybank";
+    String DB_USERNAME = "root";
+    String DB_PASSWORD = "1234";
+
+    public Statement createConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        Statement stmt = conn.createStatement();
+        return stmt;
+    }
+}
